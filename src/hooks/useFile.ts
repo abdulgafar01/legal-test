562b9697317f0dae6f6ceb29439d3ac07d45824d
Tawanda Andrew Msengezi <xrthemc@gmail.com>
2025-09-25 09:25:44 +0200

    Remove comments

--- src/hooks/useFile.ts
+++ src/hooks/useFile.ts
@@ -5,23 +5,6 @@ export interface DocumentUploadInterface {
   description: string;
 }
 
-// export const useUploadDocument = (certificationPk: string = "2") => {
-//   return useMutation({
-//     mutationFn: async (payload: DocumentUploadInterface) => {
-//       const response = await instance.post(
-//         `/certifications/${certificationPk}/files`,
-//         payload,
-//         {
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//         }
-//       );
-//       return response.data;
-//     },
-//   });
-// };
-
 export const uploadDocument = async (data: DocumentUploadInterface) => {
   const response = await instance.post("/upload", data, {
     headers: {
